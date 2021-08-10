1c73b82de1388abb7832a6a94c46573f16a5cea9
Haerun PC <haerunn@gmail.com>
2021-08-10 14:37:39 +0800

    add: modules for operator user

---
+++ application/modules/operator_user/controllers/Dashboard.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends MX_Controller {
+
+	public function index()
+	{
+		echo "hello from operator modules";
+	}
+
+}
+
+/* End of file Dashboard.php */
+/* Location: ./application/modules/operator_user/controllers/Dashboard.php */
