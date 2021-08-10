b3892779c33fab7bf0e5423e10c781b2fed802a3
Haerun PC <haerunn@gmail.com>
2021-08-10 14:37:55 +0800

    feat: route for modules operator

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,6 +1,16 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+$route['default_controller'] = 'welcome';
+$route['404_override'] = '';
+
+// **
+// modules
+$route['operator/dashboard']['get'] = 'operator_user/dashboard';
+$route['common/dashboard']['get'] = 'operator_user/dashboard';
+
+$route['translate_uri_dashes'] = TRUE;
+
 /*
 | -------------------------------------------------------------------------
 | URI ROUTING
@@ -49,6 +59,3 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
-$route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
