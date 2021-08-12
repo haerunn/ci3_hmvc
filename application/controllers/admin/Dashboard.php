51d7f252cdc373ed1a26e3dd0ea73c0e7ede30b8
Haerun PC <haerunn@gmail.com>
2021-08-12 21:52:46 +0800

    feat: function to create directory, and its folders

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -5,9 +5,43 @@ class Dashboard extends CI_Controller {
 
 	public function index()
 	{
-		echo "hello from Admin Dashboard";
+		$this->load->view('admin/index', $data ?? null, FALSE);
 	}
 
+	function create()
+	{
+		$post = $this->input->post();
+		print_r($post);
+		$directory = APPPATH."modules/".$post['brand'];
+
+		// **
+		// check if directory already exist
+		if ( !file_exists( $directory ) && !is_dir( $directory ) ) {
+			if (mkdir($directory)) {
+				if (mkdir($directory."/controllers")) {
+					$index_controller = fopen($directory."/controllers/Dashboard.php","w");
+					$content_controller = "<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+	public function index()
+	{
+		echo 'this is index of $post[brand]';
+	}
+
+}
+
+/* End of file Dashboard.php */
+/* Location: ./application/controllers/$post[brand]/Dashboard.php */
+?>";
+					fwrite($index_controller, $content_controller);
+				}
+				mkdir($directory."/models");
+				mkdir($directory."/views");
+			}
+		}
+	}
 }
 
 /* End of file Dashboard.php */
