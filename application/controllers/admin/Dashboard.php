bb01a6c1c5d852e8c0dbce53b4c0f62f3fc680ad
Haerun PC <haerunn@gmail.com>
2021-08-13 18:21:54 +0800

    feat: function to create new module after uplaoding zip file

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -5,41 +5,95 @@ class Dashboard extends CI_Controller {
 
 	public function index()
 	{
+		$data['form_url'] = base_url()."admin";
 		$this->load->view('admin/index', $data ?? null, FALSE);
 	}
 
 	function create()
 	{
-		$post = $this->input->post();
-		print_r($post);
-		$directory = APPPATH."modules/".$post['brand'];
+		try {
+			$post = $this->input->post();
+			$directory = APPPATH."modules/".$post['brand'];
 
-		// **
-		// check if directory already exist
-		if ( !file_exists( $directory ) && !is_dir( $directory ) ) {
-			if (mkdir($directory)) {
-				if (mkdir($directory."/controllers")) {
-					$index_controller = fopen($directory."/controllers/Dashboard.php","w");
-					$content_controller = "<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+			// **
+			// check if directory already exist
+			if ( !file_exists( $directory ) && !is_dir( $directory ) ) {
+				// **
+				// create directory if not exist
+				if (mkdir($directory)) {
+					// **
+					// continue to create controllers folder, and its index controllers
+					if (mkdir($directory."/controllers")) {
+						$index_controller = fopen($directory."/controllers/Dashboard.php","w");
+						$content_controller = "<?php
+						defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Dashboard extends CI_Controller {
+						class Dashboard extends CI_Controller {
 
-	public function index()
-	{
-		echo 'this is index of $post[brand]';
-	}
+							public function index()
+							{
+								\$this->load->view('index', \$data ?? null, FALSE);
+							}
 
-}
+						}
 
-/* End of file Dashboard.php */
-/* Location: ./application/controllers/$post[brand]/Dashboard.php */
-?>";
-					fwrite($index_controller, $content_controller);
+						/* End of file Dashboard.php */
+						/* Location: ./application/controllers/$post[brand]/Dashboard.php */
+						?>";
+						fwrite($index_controller, $content_controller);
+					}
+					mkdir($directory."/models");
+					mkdir($directory."/views");
+
+					// **
+					// create assets directory
+					mkdir($directory."/assets/");
+
+					// **
+					// create uploads directory
+					$dir_uploads = mkdir($directory."/uploads");
+					if (!$dir_uploads) {
+						throw new Exception('Uploads directory failed to create');
+					}
+
+					// **
+					// upload file
+					$config['upload_path'] = $directory."/uploads";
+					$config['allowed_types'] = 'zip';
+					$config['max_size']  = '3000';
+					
+					$this->load->library('upload', $config);
+					
+					if ( ! $this->upload->do_upload('file')){
+						$error = array('error' => $this->upload->display_errors());
+					}
+					else{
+						$data = array('upload_data' => $this->upload->data());
+						$zip = new ZipArchive;
+
+						// **
+						// unzip uplaoded file
+						if ($zip->open($data['upload_data']['full_path']) === TRUE) {
+							$zip->extractTo($directory."/uploads/unzipped");
+							$zip->close();
+
+							// **
+							// define directory for unzipped file
+							$dir_unzip = $directory."/uploads/unzipped/".$data['upload_data']['raw_name'];
+							
+							// **
+							// copy assets from unzipped directory to directory brand assets
+							rcopy($dir_unzip."/assets/", $directory."/assets/");
+							
+							// **
+							// move index.php to view
+							rename($dir_unzip."/index.php", $directory."/views/index.php");
+						}
+					}
 				}
-				mkdir($directory."/models");
-				mkdir($directory."/views");
-			}
+			}			
+		} catch (Exception $e) {
+			throw new Exception($e->getMessage());
 		}
 	}
 }
