1cad7621cb63d92d615c8e0bcb1e4c86ac2d6cf6
Haerun PC <haerunn@gmail.com>
2021-08-13 18:20:52 +0800

    refactor: set default controller to be dashboard. add route to submit form

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,9 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'dashboard';
 $route['404_override'] = '';
 
+// **
+// usual controller
+$route['admin']['get'] = 'admin/dashboard';
+$route['admin']['post'] = 'admin/dashboard/create';
 // **
 // modules
 $route['operator/dashboard']['get'] = 'operator_user/dashboard';
