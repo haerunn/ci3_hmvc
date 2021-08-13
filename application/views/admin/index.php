431cf6e1164e9e77365add37c2c9b24b53cf2524
Haerun PC <haerunn@gmail.com>
2021-08-13 19:25:51 +0800

    layout: move file element

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,5 +1,5 @@
 <?= form_open_multipart($form_url, '', $hidden ?? null); ?>
 	<input type="text" name="brand">
+	<input type="file" name="file">
 	<input type="submit" name="Submit" value="submit">
-		<input type="file" name="file">
 <?= form_close(); ?>
